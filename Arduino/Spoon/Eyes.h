28d68bd545f3a51c5c94bbdbd739daf4f8b5f500
Seb Lee-Delisle <sebleedelisle@gmail.com>
2015-07-07 14:06:19 +0100

    Added eyes animation to Spoon.

---
+++ Arduino/Spoon/Eyes.h
@@ -0,0 +1,160 @@
+#define MATRIX_BRIGHTNESS 5 // can be 0 to 15 I think? 
+
+Adafruit_8x8matrix matrix;
+
+// for the eyes animation
+unsigned long lastFrame = 0;
+unsigned long frameCount = 0;
+
+int nextBlinkCountdown = 10;
+int eyeMoveCountdown = 10;
+float eyeX = 3, eyeY = 3, targetEyeX = 2, targetEyeY = 2;
+
+
+void initEyes() {
+
+  Serial.begin(115200);
+  matrix.begin(0x70);
+  matrix.setBrightness(MATRIX_BRIGHTNESS);
+}
+
+void updateEyes() { 
+  
+   if ((unsigned long) (millis() - lastFrame) > 10) { // 1000/10  = 100fps
+    updateEyesAnimation();
+  }
+  
+}
+
+
+
+void updateEyesAnimation() {
+
+  lastFrame = millis();
+
+  // things that are affected by energy :
+  // how open the eyes are 0-
+  // how fast the pupil moves
+  // how fast the eyes blink
+  // how frequently the eyes blink
+
+  int eyeMoveProgress = -1;
+  boolean eyeMoving = false;
+  int eyeMoveFrames = 5;
+  if (eyeMoveCountdown < eyeMoveFrames) {
+    eyeMoveProgress = eyeMoveFrames - eyeMoveCountdown;
+    eyeMoving = true;
+  }
+
+
+  int blinkProgress = -1;
+  int blinkFrames = 14;
+  if (nextBlinkCountdown < blinkFrames) {
+    blinkProgress = blinkFrames - nextBlinkCountdown;
+  }
+
+  boolean blinking = false;
+  if (blinkProgress >= 0) blinking = true;
+
+  int top = (blinkProgress < blinkFrames / 2) ? blinkProgress : blinkFrames - blinkProgress;
+
+  energy = map(cos(radians(frameCount * 0.1)) * 100, -100, 100, 14, 0);
+  int blinkspeed = map(energy, 0, 14, 6, 1);
+
+  // sleepy determines how sleepy we are 0 to 7 (7 is asleep);
+  int sleepy = map(energy, 1, 14, 6, 0);
+  if (energy == 0) sleepy = 7;
+
+  if (top < sleepy) top = sleepy;
+  //Serial.println(sleepy);
+
+  // so it doesn't blink if the eyes are asleep
+  if (sleepy >= 7) top = 7;
+
+  matrix.clear();
+
+  if ((top <= 8) && (sleepy < 6)) {
+
+    //full screen eyeball
+    matrix.fillRect(0, top, 8, 8 - top, LED_ON);
+
+    // pupil
+    float eyey = eyeY;
+    float eyex = eyeX;
+
+    if (eyeMoving) {
+      Serial.println( (float)((float)eyeMoveProgress / (float)eyeMoveFrames));
+      eyex = (float)(targetEyeX - eyeX) * (float)((float)eyeMoveProgress / (float)eyeMoveFrames);
+      eyey = (float)(targetEyeY - eyeY) * (float)((float)eyeMoveProgress / (float)eyeMoveFrames);
+      eyex += eyeX;
+      eyey += eyeY;
+
+    }
+
+    // for rolling eyes
+    //    eyex = 3.0f + cos(radians(frameCount*12))*1.5f;
+    //    eyey = 3.0f + sin(radians(frameCount*12))*1.5f;
+
+
+    if (sleepy + 1 > eyey) {
+      eyex = constrain(eyex, 2, 3);
+      eyey = constrain(sleepy + 1, 0, 4);
+    }
+    matrix.fillRect(eyex, eyey, 3, 3, LED_OFF);
+
+    if (top >= 1) {
+      matrix.fillRect(0, top, 8, 1, LED_ON);
+    }
+
+    matrix.drawPixel(0, 0, LED_OFF);
+    matrix.drawPixel(7, 0, LED_OFF);
+    matrix.drawPixel(7, 7, LED_OFF);
+    matrix.drawPixel(0, 7, LED_OFF);
+
+  }
+  else {
+    //asleep!
+    matrix.drawLine(0, 5, 1, 6, LED_ON);
+    matrix.drawLine(1, 6, 6, 6, LED_ON);
+    matrix.drawLine(6, 6, 7, 5, LED_ON);
+
+  }
+  matrix.writeDisplay();
+
+  if (!blinking) {
+    nextBlinkCountdown--;
+  }
+  else {
+    if (frameCount % blinkspeed == 0) nextBlinkCountdown--;
+  }
+  if (nextBlinkCountdown < 0) {
+    nextBlinkCountdown = random(map(energy, 0, 14, 200, 30), map(energy, 0, 14, 800, 200));
+  }
+
+  eyeMoveCountdown--;
+  if (eyeMoveCountdown < 0) {
+    eyeX = targetEyeX;
+    eyeY = targetEyeY;
+
+    eyeMoveCountdown = random(map(energy, 0, 14, 200, 30), map(energy, 0, 14, 800, 200));
+    boolean middle =  (random(100) < 20);
+    if (middle) {
+
+      targetEyeX = 3;
+      targetEyeY = 3;
+    }
+    else {
+      float angle = random(0, 360);
+      float distance = 1.2f; //(float) (random(30,100)/100.0f) * 3; //
+      targetEyeX = 3.0f + cos(radians(angle)) * distance;
+      targetEyeY = 3.0f + sin(radians(angle)) * distance;
+
+
+    }
+  }
+
+  frameCount++;
+
+}
+
+
--- Arduino/Spoon/Spoon.ino
+++ Arduino/Spoon/Spoon.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <HSBColor.h>
 
+
 #define XAXIS_INPUT A1
 #define YAXIS_INPUT A2
 #define ZAXIS_INPUT A3
@@ -14,135 +15,128 @@
 #define NUM_LEDS 8
 #define MAX_ENERGY 14
 #define DEMO_SWITCH_PIN 13
-#define LED_BRIGHTNESS 20 // from 0 to 255
+#define LED_STRIP_BRIGHTNESS 20 // from 0 to 255
 #define DEMO_DISSIPATE_TIME 1000 // 3 seconds to dissipate each energy bar in demo mode
 #define DISSIPATE_TIME 60000 * 30 // 30 minutes to dissipate in normal mode
+
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
 int timer = 0; // timer goes from 0 to 1000 (arbitrary)
-unsigned long startTime = 0; 
-unsigned long lastSpoonMoveTime =  0; 
-long dissipateTimeMils = DISSIPATE_TIME; // 30 seconds per energy drop 
-bool spoonDown = false; 
-
-
-
-
-
+unsigned long startTime = 0;
+unsigned long lastSpoonMoveTime =  0;
+long dissipateTimeMils = DISSIPATE_TIME; // 30 seconds per energy drop
+bool spoonDown = false;
 
 // energy goes from 0 to 14 - starting at 2 ensures first light is always on
-int energy = 0; 
-
+int energy = 0;
 int mode = MODE_EAT;
 
-
-
-
 int rgb[] = {
-  0,0,0}; 
+  0, 0, 0
+};
 
 void setup() {
-  
-  pinMode(XAXIS_INPUT, INPUT); 
-  pinMode(YAXIS_INPUT, INPUT); 
-  pinMode(ZAXIS_INPUT, INPUT); 
-  pinMode(DEMO_SWITCH_PIN, INPUT_PULLUP); 
-  
-  Serial.begin(115200); 
-  
+
+  pinMode(XAXIS_INPUT, INPUT);
+  pinMode(YAXIS_INPUT, INPUT);
+  pinMode(ZAXIS_INPUT, INPUT);
+  pinMode(DEMO_SWITCH_PIN, INPUT_PULLUP);
+
+  Serial.begin(115200);
+
   strip.begin();
-  updateLedStrip(); 
-  
+  updateLedStrip();
+
 }
 
 void loop() {
 
-   
+  updateEyes();
 
-  if(mode == MODE_DISSIPATE) { 
-    dissipateTimeMils = digitalRead(DEMO_SWITCH_PIN)?DEMO_DISSIPATE_TIME:DISSIPATE_TIME; 
-    if((unsigned long)(millis()-startTime) > dissipateTimeMils) {
-      startTime = millis(); 
+  if (mode == MODE_DISSIPATE) {
+    dissipateTimeMils = digitalRead(DEMO_SWITCH_PIN) ? DEMO_DISSIPATE_TIME : DISSIPATE_TIME;
+    if ((unsigned long)(millis() - startTime) > dissipateTimeMils) {
+      startTime = millis();
       energy--;
-      if(energy<0) energy =0; 
-     
+      if (energy < 0) energy = 0;
+
     }
-     updateLedStrip(); 
+    updateLedStrip();
   }
- 
-  if((mode==MODE_EAT) && checkAccelerometer()) updateLedStrip(); 
-  
+
+  if ((mode == MODE_EAT) && checkAccelerometer()) updateLedStrip();
+
 }
 
 
-void startDissipating() { 
-  
-  for(int j = 0; j<5; j++) { 
-    for(int i = 0; i<NUM_LEDS; i++ ) { 
-        strip.setPixelColor(i, strip.Color(0,255,0)); 
-        strip.show();
-       delay(20);  
+void startDissipating() {
+
+  for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < NUM_LEDS; i++ ) {
+      strip.setPixelColor(i, strip.Color(0, 255, 0));
+      strip.show();
+      delay(20);
     }
-    for(int i = 0; i<NUM_LEDS; i++ ) { 
-        strip.setPixelColor(i, strip.Color(0,LED_BRIGHTNESS,0)); 
-        strip.show(); 
-        delay(20); 
+    for (int i = 0; i < NUM_LEDS; i++ ) {
+      strip.setPixelColor(i, strip.Color(0, LED_STRIP_BRIGHTNESS, 0));
+      strip.show();
+      delay(20);
     }
   }
-  startTime = millis(); 
-  mode = MODE_DISSIPATE; 
-  
-}
-// checks the accelerometer and returns true if it's changed. 
+  startTime = millis();
+  mode = MODE_DISSIPATE;
 
-boolean checkAccelerometer() { 
-  int yAxis = analogRead(YAXIS_INPUT); 
+}
+// checks the accelerometer and returns true if it's changed.
+
+boolean checkAccelerometer() {
+  int yAxis = analogRead(YAXIS_INPUT);
   // debounce
-  if((unsigned long)(millis() - lastSpoonMoveTime) > 400) { 
-    
-    if(spoonDown && (yAxis<320)) { 
-       spoonDown = false; 
-      energy++;  
-      if(energy>=MAX_ENERGY) {     
-        startDissipating(); 
-      } 
-      lastSpoonMoveTime = millis(); 
-      Serial.println("ENERGY ++ "); 
+  if ((unsigned long)(millis() - lastSpoonMoveTime) > 400) {
+
+    if (spoonDown && (yAxis < 320)) {
+      spoonDown = false;
+      energy++;
+      if (energy >= MAX_ENERGY) {
+        startDissipating();
+      }
+      lastSpoonMoveTime = millis();
+      Serial.println("ENERGY ++ ");
       return true;
-    } else if((!spoonDown) && (yAxis>400)) { 
-      spoonDown = true; 
-      lastSpoonMoveTime = millis(); 
-    } 
+    } else if ((!spoonDown) && (yAxis > 400)) {
+      spoonDown = true;
+      lastSpoonMoveTime = millis();
+    }
   }
-  return false; 
-  
+  return false;
+
 }
 
 
-void updateLedStrip() { 
-  
-  Serial.println("updateLedStrip"); 
-  
-  int currentLevelPixel = constrain(energy+1, 0, MAX_ENERGY+2); 
-  
+void updateLedStrip() {
+
+  Serial.println("updateLedStrip");
+
+  int currentLevelPixel = constrain(energy + 1, 0, MAX_ENERGY + 2);
+
   Serial.println(currentLevelPixel);
-  for(uint16_t i=0; i<NUM_LEDS*2; i++) {
-    if(i<=currentLevelPixel) strip.setPixelColor(i/2, strip.Color((i%2==0) ? LED_BRIGHTNESS : 0, LED_BRIGHTNESS, 0));
-    else if(i%2==0)strip.setPixelColor(i/2, strip.Color(0, 0, 0));
-  } 
-  
+  for (uint16_t i = 0; i < NUM_LEDS * 2; i++) {
+    if (i <= currentLevelPixel) strip.setPixelColor(i / 2, strip.Color((i % 2 == 0) ? LED_STRIP_BRIGHTNESS : 0, LED_STRIP_BRIGHTNESS, 0));
+    else if (i % 2 == 0)strip.setPixelColor(i / 2, strip.Color(0, 0, 0));
+  }
+
   strip.show();
- 
-  
+
+
 }
 
 
-void setPixelHSB(int pixelnum, int hue, int saturation, int brightness) { 
+void setPixelHSB(int pixelnum, int hue, int saturation, int brightness) {
 
-  while(hue<0) hue+=360; 
-  while(hue>360) hue-=360; 
-  // H2R_HSBtoRGB( hue, saturation, brightness, &rgb[0]); 
+  while (hue < 0) hue += 360;
+  while (hue > 360) hue -= 360;
+  // H2R_HSBtoRGB( hue, saturation, brightness, &rgb[0]);
   // strip.setPixelColor(pixelnum, strip.Color(rgb[0], rgb[1], rgb[2]));
 
 }
